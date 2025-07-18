203f91892361a613f0b096698a3641a16a4f672c
Tarik Ermis <tarik@n3tz.io>
2025-07-18 17:58:00 +0200

    feat: update projects

--- components/ProjectsCard.jsx
+++ components/ProjectsCard.jsx
@@ -19,37 +19,56 @@ const ProjectsCard = ({ data }) => {
               <div className="pl-4">
                 <h3>{data.name}</h3>
                 <p className="description mt-3">{data.desc}</p>
-                {data.github ? (
-                  <Button
-                    className="btn-icon"
-                    color="github"
-                    href={data.github}
-                    target="_blank"
-                    rel="noopener"
-                    aria-label="Github"
-                  >
-                    <span className="btn-inner--icon">
-                      <i className="fa fa-github" />
-                    </span>
-                  </Button>
-                ) : null}
-                {data.link ? (
-                  <Button
-                    className="btn-icon"
-                    color="primary"
-                    href={data.link}
-                    target="_blank"
-                    rel="noopener"
-                    aria-label="Twitter"
-                  >
-                    <span className="btn-inner--icon">
-                      <i className="fa fa-arrow-right mr-2" />
-                    </span>
-                    <span className="nav-link-inner--text ml-1">
-                      Visit Website
-                    </span>
-                  </Button>
-                ) : null}
+                {data.achievements && data.achievements.length > 0 && (
+                  <div className="mt-4">
+                    <h5 className="text-info mb-3">Key Achievements:</h5>
+                    <ul className="list-unstyled">
+                      {data.achievements.map((achievement, index) => (
+                        <li key={index} className="py-2">
+                          <div className="d-flex align-items-start">
+                            <div className="icon icon-xs icon-shape bg-gradient-primary shadow rounded-circle text-white mr-3 mt-1">
+                              <i className="fa fa-check"></i>
+                            </div>
+                            <span className="description">{achievement}</span>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+                <div className="mt-4">
+                  {data.github ? (
+                    <Button
+                      className="btn-icon mr-2"
+                      color="github"
+                      href={data.github}
+                      target="_blank"
+                      rel="noopener"
+                      aria-label="Github"
+                    >
+                      <span className="btn-inner--icon">
+                        <i className="fa fa-github" />
+                      </span>
+                    </Button>
+                  ) : null}
+                  {data.link ? (
+                    <Button
+                      className="btn-icon"
+                      color="primary"
+                      href={data.link}
+                      target="_blank"
+                      rel="noopener"
+                      aria-label="Visit Website"
+                    >
+                      <span className="btn-inner--icon">
+                        <i className="fa fa-arrow-right mr-2" />
+                      </span>
+                      <span className="nav-link-inner--text ml-1">
+                        Visit Website
+                      </span>
+                    </Button>
+                  ) : null}
+                </div>
               </div>
             </div>
           </CardBody>
