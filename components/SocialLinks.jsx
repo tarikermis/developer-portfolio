d4e2d2160b52ca29015c346652f1a90cac8ddb1c
Tarik Ermis <tarik@n3tz.io>
2025-12-15 18:00:40 +0100

    fix: imprint link

--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -6,57 +6,56 @@ import { socialLinks } from "../portfolio";
 
 const SocialLinks = () => {
 	return (
-		<div className="btn-wrapper text-lg">
-			<Button
-				className="btn-icon-only rounded-circle"
-				color="twitter"
-				href={socialLinks.twitter}
-				target="_blank"
-				rel="noopener"
-				aria-label="Twitter"
-			>
-				<span className="btn-inner--icon">
-					<i className="fa fa-twitter" />
-				</span>
-			</Button>
+    <div className="btn-wrapper text-lg">
+      <Button
+        className="btn-icon-only rounded-circle"
+        color="twitter"
+        href={socialLinks.twitter}
+        target="_blank"
+        rel="noopener"
+        aria-label="Twitter"
+      >
+        <span className="btn-inner--icon">
+          <i className="fa fa-twitter" />
+        </span>
+      </Button>
 
-			<Button
-				className="btn-icon-only rounded-circle ml-1"
-				color="github"
-				href={socialLinks.github}
-				rel="noopener"
-				aria-label="Github"
-				target="_blank"
-			>
-				<span className="btn-inner--icon">
-					<i className="fa fa-github" />
-				</span>
-			</Button>
-			<Button
-				className="btn-icon-only rounded-circle ml-1"
-				color="twitter"
-				rel="noopener"
-				aria-label="Linkedin"
-				href={socialLinks.linkedin}
-				target="_blank"
-			>
-				<span className="btn-inner--icon">
-					<i className="fa fa-linkedin" />
-				</span>
-			</Button>
-			<Button
-				className="btn-icon-only rounded-circle ml-1"
-				color="github"
-				rel="noopener"
-				aria-label="imprint"
-				href="/imprint"
-			>
-				<span className="btn-inner--icon">
-					§
-				</span>
-			</Button>
-		</div>
-	);
+      <Button
+        className="btn-icon-only rounded-circle ml-1"
+        color="github"
+        href={socialLinks.github}
+        rel="noopener"
+        aria-label="Github"
+        target="_blank"
+      >
+        <span className="btn-inner--icon">
+          <i className="fa fa-github" />
+        </span>
+      </Button>
+      <Button
+        className="btn-icon-only rounded-circle ml-1"
+        color="twitter"
+        rel="noopener"
+        aria-label="Linkedin"
+        href={socialLinks.linkedin}
+        target="_blank"
+      >
+        <span className="btn-inner--icon">
+          <i className="fa fa-linkedin" />
+        </span>
+      </Button>
+      <Button
+        className="btn-icon-only rounded-circle ml-1"
+        color="github"
+        rel="noopener"
+        aria-label="imprint"
+        target="_blank"
+        href="https://n3tz.io/impressum"
+      >
+        <span className="btn-inner--icon">§</span>
+      </Button>
+    </div>
+  );
 };
 
 export default SocialLinks;
